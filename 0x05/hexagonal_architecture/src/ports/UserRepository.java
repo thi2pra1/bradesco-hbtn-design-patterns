d6d145e09c3b53c3a0660180afeda201811b259b
Thiago Carneiro <thiagoc.silva@bradesco.com.br>
2025-10-05 15:50:51 -0300

    commit

---
+++ 0x05/hexagonal_architecture/src/ports/UserRepository.java
@@ -0,0 +1,10 @@
+// Porta de saída (Interface para persistência)
+package ports;
+import domain.User;
+import java.util.List;
+
+public interface UserRepository {
+    void save(User user);
+    User findById(String id);
+    List<User> findAll();
+}
